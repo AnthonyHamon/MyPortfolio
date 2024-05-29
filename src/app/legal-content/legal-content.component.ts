18ea5800f3dd12227dcee0e8455a158ff026fd25
Anthony Hamon <anthony.hamon91@gmail.com>
2024-05-29 13:50:58 +0200

    above the fold component done

---
+++ src/app/legal-content/legal-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './legal-content.component.html',
+  styleUrl: './legal-content.component.scss'
+})
+export class LegalContentComponent {
+
+}
--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NavbarComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
