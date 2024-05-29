6e538f99525418dfc6c5fad312c45c28f0e82eb8
Anthony Hamon <anthony.hamon91@gmail.com>
2024-05-29 05:43:23 +0200

    changed project structure

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from '../../shared/header/navbar/navbar.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
