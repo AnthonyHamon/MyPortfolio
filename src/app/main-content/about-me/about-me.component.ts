f37a9a17237d69e1dae00b33ca8d0ba26998ed48
Anthony Hamon <anthony.hamon91@gmail.com>
2024-05-30 10:04:21 +0200

    about me section done

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SocialLinksBarComponent } from './social-links-bar/social-links-bar.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [SocialLinksBarComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
