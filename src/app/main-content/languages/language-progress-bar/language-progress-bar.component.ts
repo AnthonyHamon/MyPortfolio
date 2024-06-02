bd05268f17adc4179b38f4d56b08e3dd3b83c2ac
Anthony Hamon <anthony.hamon91@gmail.com>
2024-06-02 08:59:17 +0200

    added language component

---
+++ src/app/main-content/languages/language-progress-bar/language-progress-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-progress-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './language-progress-bar.component.html',
+  styleUrl: './language-progress-bar.component.scss'
+})
+export class LanguageProgressBarComponent {
+
+}
