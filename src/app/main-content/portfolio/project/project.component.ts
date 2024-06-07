993a4d55c10bb1d2e3c6dedbe4cb82af80059506
Anthony Hamon <anthony.hamon91@gmail.com>
2024-06-07 05:43:13 +0200

    achieved project animation

--- src/app/main-content/portfolio/project/project.component.ts
+++ src/app/main-content/portfolio/project/project.component.ts
@@ -1,13 +1,30 @@
+import { animate, state, style, trigger, transition } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project.component.html',
-  styleUrl: './project.component.scss'
+  styleUrl: './project.component.scss',
+  animations: [
+    trigger('slideInOutAnimation', [
+      state('open', style({
+        width: '100%',
+      })),
+      state('closed', style({
+        width: '30%',
+      })),
+      transition('open <=> closed', [
+        animate('0.4s')
+      ])
+    ])
+  ],
 })
 export class ProjectComponent {
+
+  isOpen: boolean = false;
 
   @Input() project = {
     projectName: 'Fantasia Adventure',
@@ -16,4 +33,11 @@ export class ProjectComponent {
     githubLink: '',
     usedTechnologies: 'JavaScript, HTML, CSS',
   };
+
+
+
+toggleProjectDescription(){
+  this.isOpen = !this.isOpen;
+}
+
 }
