57550d7c30542375fa1b8000e48fadfb5b537292
Anthony Hamon <anthony.hamon91@gmail.com>
2024-06-18 16:38:41 +0200

    added legalcontent folder + imprint and privacy policy component

--- sendMail.php
+++ sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'h.anthony@outlook.de';  
+            $recipient = 'DEINE@MAIL.de';  
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
@@ -27,7 +27,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $headers[] = 'Content-type: text/html; charset=utf-8';
 
             // Additional headers
-            $headers[] = "From: noreply@anthony-hamon.com";
+            $headers[] = "From: noreply@mywebsite.com";
 
             mail($recipient, $subject, $message, implode("\r\n", $headers));
             break;
--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -13,8 +13,8 @@ export class PortfolioComponent {
   portfolio = [
     {
       projectName: 'Fantasia Adventure',
-      projectPicture: 'assets/img/projects/f-a-gaming-display.png',
-      liveDemoLink: 'https://fantasia-adventure/anthony-hamon.com/',
+      projectPicture: 'assets/img/projects/fa-game-screen-console3.jpg',
+      liveDemoLink: 'https://fantasia-adventure.anthony-hamon.com/',
       githubLink: 'https://github.com/AnthonyHamon/Fantasia-Adventures',
       usedTechnologies: 'JavaScript, HTML, CSS',
       projectDescription: 'A 2D Platform, jump and run adventure game mainly based on JavaScript and OOP. Be a Hero, collect items, beat all creatures and defeat the Boss using differents skills and save the World!',
@@ -22,8 +22,8 @@ export class PortfolioComponent {
     },
     {
       projectName: 'Join',
-      projectPicture: 'assets/img/projects/phone-hand.png',
-      liveDemoLink: 'https://join/anthony-hamon.com/',
+      projectPicture: 'assets/img/projects/join-phone.jpg',
+      liveDemoLink: 'https://join.anthony-hamon.com/',
       githubLink: 'https://github.com/AnthonyHamon/Join-GruppenProjekt',
       usedTechnologies: 'Angular, TypeScript, CSS, Firebase',
       projectDescription: 'Log in and use this task manager app inspired by the Kanban System. Create, update and deletes your tasks. Assign them to users and organize your project between categories and status',
@@ -31,21 +31,30 @@ export class PortfolioComponent {
     },
     {
       projectName: 'Pokédex',
-      projectPicture: 'assets/img/projects/pokedex-laptop-reverse.png',
-      liveDemoLink: 'https://pokedex/anthony-hamon.com/',
+      projectPicture: 'assets/img/projects/pokedex-reverse.jpg',
+      liveDemoLink: 'https://pokédex.anthony-hamon.com/',
       githubLink: 'https://github.com/AnthonyHamon/Pokedex',
       usedTechnologies: 'JavaScript, HTML, CSS, API',
       projectDescription: 'A library for all Pokémons. Obtain many Informations about the Pocket Monsters or search for a specific one. Based ond the PokéAPI',
 
     },
     {
-      projectName: 'Simple CRM',
-      projectPicture: 'assets/img/projects/laptop.png',
-      liveDemoLink: 'https://da-bubble/anthony-hamon.com/',
+      projectName: 'Portfolio',
+      projectPicture: 'assets/img/projects/portfolio.jpg',
+      liveDemoLink: 'https://anthony-hamon.com/',
       githubLink: 'https://github.com/AnthonyHamon/MyPortfolio',
-      usedTechnologies: 'Angular, Firebase',
-      projectDescription: 'A very Simple Customer Relationship Management system working with CRUD functionality.',
+      usedTechnologies: 'Angular, TypeScript, SCSS',
+      projectDescription: 'A simple one Page portfolio to introduce myself and share my projects to potential client / company / recruiter.',
 
-    }
+    },
+    // {
+    //   projectName: 'Simple CRM',
+    //   projectPicture: 'assets/img/projects/portfolio.jpg',
+    //   liveDemoLink: 'https://da-bubble.anthony-hamon.com/',
+    //   githubLink: 'https://github.com/AnthonyHamon/MyPortfolio',
+    //   usedTechnologies: 'Angular, Firebase',
+    //   projectDescription: 'A very Simple Customer Relationship Management system working with CRUD functionality.',
+
+    // }
   ]
 }
