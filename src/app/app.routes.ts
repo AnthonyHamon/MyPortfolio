2ad67fcf585d6efb3a1e3307ff2125ee71c12617
Anthony Hamon <anthony.hamon91@gmail.com>
2024-06-19 16:34:05 +0200

    added imprint styled it and added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './legal-content/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './legal-content/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    {'path': '', component:MainContentComponent },
+    {'path': '', component: MainContentComponent },
+    {'path': 'imprint', component: ImprintComponent},
+    {'path': 'privacy-policy', component: PrivacyPolicyComponent},
 ];
